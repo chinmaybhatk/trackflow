c9dd8f9eeb7bcb3198381e9529881c87a1fba950
chinmaybhatk <chinmaybhatk@gmail.com>
2025-08-05 19:26:02 +0530

    Create Campaign Link Variant __init__.py

---
+++ trackflow/trackflow/doctype/campaign_link_variant/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2024, chinmaybhatk and contributors
+# For license information, please see license.txt
+
+from .campaign_link_variant import CampaignLinkVariant
