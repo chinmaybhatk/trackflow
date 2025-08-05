29ca55b7d526bc2794e8d83233245b1040190a0d
chinmaybhatk <chinmaybhatk@gmail.com>
2025-08-05 22:17:01 +0530

    Create remaining missing child DocTypes for TrackFlow

---
+++ trackflow/trackflow/doctype/lead_status_change/lead_status_change.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, TrackFlow and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+class LeadStatusChange(Document):
+    pass
